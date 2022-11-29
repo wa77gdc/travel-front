c258ae986d48643b591bb8153ee15233a6dde90f
wa77gdc <wa77gdc@gmail.com>
2022-11-29 10:42:12 +0700

    fist

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+
+function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+    fetch('https://tasty-lion-glasses.cyclic.app/attractions')
+      .then(res=>res.json())
+      .then((result)=>{
+        setItems(result)
+      })
+  },[])
+  return (
+    <div>
+      <h1>Attractions</h1>
+      <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          {item.name}<br/>
+          {item.detail}<br />
+          <img src={item.coverimage} alt={item.name} />
+        </li>
+      ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
